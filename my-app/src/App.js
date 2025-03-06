27067b8bf08bf6812998f72e4ba2856873feb0eb
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-03-06 21:04:57 +0300

    added the card for the post

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,15 @@ function App() {
   return (
     <div className="App">
      
+    <div class="card" style="width: 18rem;">
+  <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/152/274/original/Agriculture_farming_in_Viet_Nam_.jpg?1733646648" class="card-img-top" alt="post-image" />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 
-<h1>
-  Hello world
-</h1>
 
 
     </div>

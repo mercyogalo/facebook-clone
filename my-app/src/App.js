4b5dc8cd6cbf335060b4488c1cd5aed9f6e6458a
mercyogalo <143324456+mercyogalo@users.noreply.github.com>
2025-03-06 21:38:02 +0300

    created the function to handle click

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,33 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+let [counter, setCounter]=useState(0);
+let [click,setClick]=useState(0)
+
+  function handleClick(){
+setCounter(counter++);
+setClick(click++);
+
+
+if (click>1){
+  setCounter(counter--);
+}
+
+
+  }
+
+
+
+
   return (
     <div className="App">
      
-    <div class="card" style="width: 18rem;">
-  <img src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/152/274/original/Agriculture_farming_in_Viet_Nam_.jpg?1733646648" class="card-img-top" alt="post-image" />
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
 
+<button className="mt-2 bg-danger text-light" onClick={handleClick}>
+Like {counter}
+</button>
 
 
     </div>
